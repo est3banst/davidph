90abb7a9b09414a2deca79cb82334cd8ecd2f333
est3banst <estebeeen@outlook.com>
2024-11-13 00:48:08 -0300

    f s t

--- src/componentes/Card.jsx
+++ src/componentes/Card.jsx
@@ -10,10 +10,10 @@ const Card = ({ card }) => {
         <div
           style={{
             backgroundImage: `url(${card.url})`,
-            backgroundSize: "100% 100%",
+            backgroundSize: "100% 50%",
             backgroundPosition: "center center",
           }}
-          className="absolute top-0 left-0 right-0 bottom-0 p-6 h-[calc(100vh+2rem)] w-[calc(120vw+2rem)] transition-transform duration-300 group-hover:scaleX-110"
+          className="absolute top-0 left-0 right-0 bottom-0 p-6 h-[calc(110vh+1rem)] w-[110vw] md:h-[calc(100vh+2rem)] md:w-[calc(120vw+2rem)] transition-transform duration-300 group-hover:scale-120"
         ></div>
         <div className="absolute inset-0 z-10 p-6 grid place-content-center">
           <p className="bg-transparent p-8 text-2xl md:text-4xl lg:text-6xl font-black uppercase text-white">
