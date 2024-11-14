6b743cafb4c08c3adf983764a411c9c3ab944cc6
est3banst <estebeeen@outlook.com>
2024-11-13 21:44:16 -0300

    s i

--- src/componentes/Card.jsx
+++ src/componentes/Card.jsx
@@ -11,9 +11,10 @@ const Card = ({ card }) => {
           style={{
             backgroundImage: `url(${card.url})`,
             backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
             backgroundPosition: "center center",
           }}
-          className="absolute top-0 left-0 bg-[length:120%_60%] md:bg-[length:100%_80%] right-0 bottom-0 h-[calc(100vh+1rem)] w-[100vw] md:h-[calc(100vh+2rem)] md:w-[calc(120vw+2rem)] transition-transform duration-300 group-hover:scale-120"
+          className="absolute top-0 left-0 right-0 bottom-0 h-[100vh] w-[100vw] md:h-[calc(100vh+2rem)] md:w-[calc(120vw+2rem)] transition-transform duration-300 group-hover:scale-120"
         ></div>
         <div className="absolute inset-0 z-10 p-6 grid place-content-center">
           <p className="bg-transparent p-8 text-2xl md:text-4xl lg:text-6xl font-black uppercase text-white">
