945a8907ce0ca882e03bb7aea34f1b6b4e09ac6b
est3banst <estebeeen@outlook.com>
2025-01-30 11:28:55 -0300

    d

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -5,8 +5,7 @@ const Footer = () => {
     <div>
         <footer className='bg-slate-800 p-4 text-slate-50 font-extrabold'>
         <small className='text-center flex flex-col gap-3'>
-        Todas las marcas o derechos de cualquier tipo, nombres registrados, logos o insignias, usados o citados en esta web, son propiedad de sus respectivos dueños.
-            Todas las fotos provienen de <a 
+         Todas las fotos provienen de <a 
             className='underline'
             href="https://www.pexels.com">Pexels</a>
         </small>
